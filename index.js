5c01f123dd974e6bd044de32e434ac0835a2a278
Blitzableiter <max.rumford@gmx.net>
2018-11-08 06:44:52 +0100

    Initial Commit
    WIP on Emoji support and Design stuff

---
+++ index.js
@@ -0,0 +1,27 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var favicon = require("serve-favicon");
+app.use(favicon(__dirname + "/favicon.ico"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket) {
+  io.emit("new connection");
+  console.log("a user connected");
+
+  socket.on("disconnect", function() {
+    io.emit("new disconnection");
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", function(msg, usr) {
+    if (msg.trim() && usr.trim()) io.emit("chat message", msg, usr);
+  });
+});
+
+http.listen(3000, function() {
+  console.log("listening on *:3000");
+});
