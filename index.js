344f160ca67b7a7cbab8b17fcd16a9583e105a4c
Blitzableiter <max.rumford@gmx.net>
2018-11-08 15:08:51 +0100

    styling and info
    - Info message next to username-input, when none is provided
    - css in css-file, no more internal-css
    - styling

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var app = require("express")();
+var express = require("express");
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var favicon = require("serve-favicon");
+
+app.use("/public", express.static("public"));
 app.use(favicon(__dirname + "/favicon.ico"));
 
 app.get("/", function(req, res) {
